f46a8b04cd0434de9299464d1568b5c3ae2bd5cb
krasnyuk_art <krasnyuk.photo@gmail.com>
2020-05-17 13:04:40 +0300

    Added: chat messages - participant not selected placeholder

--- src/app/shared/modules/chat/components/chat-messages/chat-messages.component.ts
+++ src/app/shared/modules/chat/components/chat-messages/chat-messages.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChatParticipantsQuery} from "../../state/chat-participant/chat-participants.query";
+import {Observable} from "rxjs";
+import {fadeInOutAnimation} from "../../../../../core/animations/fade-in-out.animation";
 
 @Component({
   selector: 'app-chat-messages',
   templateUrl: './chat-messages.component.html',
   styleUrls: ['./chat-messages.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeInOutAnimation()]
 })
 export class ChatMessagesComponent implements OnInit {
+  hasSelectedParticipant$: Observable<boolean> = this.chatParticipantsQuery.hasSelectedParticipant$;
 
-  constructor() { }
+  constructor(private chatParticipantsQuery: ChatParticipantsQuery) { }
 
   ngOnInit(): void {
   }
--- src/app/shared/modules/chat/state/chat-participant/chat-participants.query.ts
+++ src/app/shared/modules/chat/state/chat-participant/chat-participants.query.ts
@@ -3,28 +3,39 @@ import {QueryEntity} from '@datorama/akita';
 import {ChatParticipantsStore, ChatParticipantsState} from './chat-participants.store';
 import {Observable} from "rxjs";
 import {ChatParticipant} from "./chat-participant.model";
-import {switchMap} from "rxjs/operators";
+import {map, switchMap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class ChatParticipantsQuery extends QueryEntity<ChatParticipantsState> {
-  participantsIsLoading$: Observable<boolean> = this.selectLoading();
-  participantsSearchFilter$: Observable<string> = this.select(state => state.ui.participantsSearchFilter);
-  participants$: Observable<Array<ChatParticipant>> = this.participantsSearchFilter$.pipe(
-    switchMap((filterValue: string | null) => {
-      if (!filterValue) {
-        return this.selectAll();
-      }
-      return this.selectAll({
-        filterBy: (participant: ChatParticipant) => {
-          return participant.userName.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase());
-        }
-      });
-    })
-  );
-  selectedParticipantId$: Observable<string | null> = this.selectActiveId();
+  participantsIsLoading$: Observable<boolean>;
+  participantsSearchFilter$: Observable<string>;
+  participants$: Observable<Array<ChatParticipant>>;
+  selectedParticipantId$: Observable<string | null>;
+  hasSelectedParticipant$: Observable<boolean>;
 
   constructor(protected store: ChatParticipantsStore) {
     super(store);
+    this.participantsIsLoading$ = this.selectLoading();
+    this.participantsSearchFilter$ = this.select(state => state.ui.participantsSearchFilter);
+    this.participants$ = this.getParticipants();
+    this.selectedParticipantId$ = this.selectActiveId();
+    this.hasSelectedParticipant$ = this.selectedParticipantId$.pipe(
+      map((selectedParticipantId: string | null) => selectedParticipantId !== null)
+    );
   }
 
+  private getParticipants() {
+    return this.participantsSearchFilter$.pipe(
+      switchMap((filterValue: string | null) => {
+        if (!filterValue) {
+          return this.selectAll();
+        }
+        return this.selectAll({
+          filterBy: (participant: ChatParticipant) => {
+            return participant.userName.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase());
+          }
+        });
+      })
+    );
+  }
 }
